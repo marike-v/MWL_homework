3f8f374e1d2a7b2e64099bd7d53c4b497bb0f7e5
marike-v <marike_vossbeck@posteo.de>
2025-06-04 16:45:55 +0200

    Delete Worksheet1 directory

--- Worksheet1/task1.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1a889c443887f7b52cd9fdaf7281ca6607bb9412643acd576322f8aa8decc527
-size 2309
--- Worksheet1/task4.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:427701b691213ad13d95fcf5056d26b36d13e8c00e1b875f88838c107ba7e3dd
-size 2182
